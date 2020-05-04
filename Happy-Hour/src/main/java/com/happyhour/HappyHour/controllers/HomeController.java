211597d87b9393d2209530b2183e74a4a1491aef
shickenphoot <un_dis_closed4@hotmail.com>
2020-05-04 18:16:04 -0500

    -search function implemented -modified model for current use -list results -temporary

--- Happy-Hour/src/main/java/com/happyhour/HappyHour/controllers/HomeController.java
+++ Happy-Hour/src/main/java/com/happyhour/HappyHour/controllers/HomeController.java
@@ -5,24 +5,19 @@ import com.happyhour.HappyHour.models.HappyHour;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.thymeleaf.model.IModel;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;
 
 @Controller
 public class HomeController {
 
     @RequestMapping("")
     public String index(Model model) {
-
+        HappyHourData data=new HappyHourData();
         model.addAttribute("title", "Test Page");
-        model.addAttribute("happyHours",HappyHourData.getHappyHours());
+        model.addAttribute("happyHours",data.getHappyHours());
         return "index";
     }
-
-
 
     @GetMapping("user-view/{hhId}")
     public String displayHH(Model model, @PathVariable int hhId){
@@ -31,7 +26,5 @@ public class HomeController {
         return "user-view";
 
     }
-
-
 
 }
