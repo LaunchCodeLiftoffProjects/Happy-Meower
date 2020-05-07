8b224aa8483de7408d863e98f3b8a39b77177dea
Jessica <jrnewport07@gmail.com>
2020-05-06 19:46:22 -0500

    deleted unneccessary mapping on authentication controller

--- Happy-Hour/src/main/java/com/happyhour/HappyHour/controllers/AuthenticationController.java
+++ Happy-Hour/src/main/java/com/happyhour/HappyHour/controllers/AuthenticationController.java
@@ -11,7 +11,6 @@ import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -20,12 +19,6 @@ import java.util.Optional;
 
 @Controller
 public class AuthenticationController {
-
-    @RequestMapping("owner-login")
-    public String index(Model model) {
-        model.addAttribute("title", "Owner Login");
-        return "owner-login";
-    }
 
     @Autowired
     OwnerRepository ownerRepository;
@@ -127,5 +120,11 @@ public class AuthenticationController {
         setOwnerInSession(request.getSession(), theOwner);
 
         return "redirect:";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request){
+        request.getSession().invalidate();
+        return "redirect:/owner-login";
     }
 }
