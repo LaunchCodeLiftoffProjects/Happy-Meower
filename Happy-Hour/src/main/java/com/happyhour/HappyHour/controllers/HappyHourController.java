ac7362df43927298242fbb6dedcbb51b9a184cfe
keitaroryu <keitaroryu@gmail.com>
2020-05-12 12:47:23 -0500

    Added address import feature to Map API from HappyHourController. Fixed a typo in results.html and removed "Google Maps DEMO" header.

--- Happy-Hour/src/main/java/com/happyhour/HappyHour/controllers/HappyHourController.java
+++ Happy-Hour/src/main/java/com/happyhour/HappyHour/controllers/HappyHourController.java
@@ -10,6 +10,7 @@ import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
 
 @Controller
 @RequestMapping
@@ -26,8 +27,18 @@ public class HappyHourController {
 
     @PostMapping("results")
     public String searchHH(Model model, @RequestParam String searchTerm){
+        ArrayList<HappyHour> searchResults = HourData.searchHappyHour(searchTerm, happyHourRepository.findAll());
+        ArrayList<String> resultAddresses = new ArrayList<>();
+
+        for (HappyHour result : searchResults) {
+            resultAddresses.add(result.getAddress());
+        }
+
         model.addAttribute("searchTerm",searchTerm);
-        model.addAttribute("happyHours",HourData.searchHappyHour(searchTerm, happyHourRepository.findAll()));
+        model.addAttribute("happyHours",searchResults);
+        model.addAttribute("zipcode", "64118");
+        model.addAttribute("addressList",resultAddresses);
+
         return "results";
     }
 
